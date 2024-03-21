b05dbe41fc1ace30fad917be0fe4ef4d42d182d9
Andejj22 <antti.t.jormanainen@hotmail.com>
2024-03-21 15:00:04 +0200

    task 4 completition

--- init_vhz.m
+++ init_vhz.m
@@ -5,3 +5,4 @@ close all;          % Close the figures
 %% Controller parameters
 T_s = 125e-6;       % Sampling period, switching frequency f_sw = 1/(2*T_s)
 psi_s_ref = 0.95;   % Stator-flux reference (or the V/Hz constant)
+T_L = 14.63; % Load torque step final value
