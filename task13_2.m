87df65c93ff45bfc212ad337b623470006bd1d8e
Mikko Saren <mikko.saren@aalto.fi>
2024-04-08 16:07:31 +0300

    task13, simulation ready

---
+++ task13_2.m
@@ -0,0 +1,37 @@
+%init_vector;
+mdl = "vector_model";
+set_param(mdl,"ReturnWorkspaceOutputs","on");
+default_sim = sim(mdl);
+
+alpha_c = 2*pi*600; % new value for current-controller bandwidth
+T_s = (125e-6)/2; % twice the sampling frequency
+task13_sim2 = sim(mdl);
+T_s = 125e-6; % sampling time back to default
+alpha_c = 2*pi*300; % default value for current-controller bandwidth
+set_param(mdl,"ReturnWorkspaceOutputs","off");
+
+%% Speed and torque
+figure;
+subplot(2, 1, 1); 
+plot(default_sim.w_M.time, n_p*default_sim.w_M.data, 'b', 'linewidth', 2); hold on; grid on; 
+stairs(default_sim.w_M_ref.time, n_p*default_sim.w_M_ref.data, 'r', 'LineWidth',1); 
+
+plot(task13_sim2.w_M.time, n_p*task13_sim2.w_M.data, '--g', 'linewidth',2);
+%stairs(task13_sim.w_M_ref.time, n_p*task13_sim.w_M_ref.data, '--m', 'linewidth',1);
+legend('Rotor speed, default', 'Reference', 'Rotor speed,\alpha_c = 2\pi600 rad/s, twice sampling frequency', ...
+    'Location','SouthEast');
+ylabel('Electr. angular speed (rad/s)');
+
+subplot(2,1,2);
+
+plot(default_sim.tau_M.time, default_sim.tau_M.data, 'b', 'linewidth', 2); hold on; grid on;   
+%stairs(task13_sim.tau_M_ref.time, task13_sim.tau_M_ref.data, '--m', 'LineWidth',1); 
+
+stairs(default_sim.tau_M_ref.time, default_sim.tau_M_ref.data, 'r', 'LineWidth',1); 
+plot(task13_sim2.tau_M.time, task13_sim2.tau_M.data, 'g', 'linewidth', 2); hold on; grid on;   
+
+
+
+legend('Electromagnetic torque, default', 'Reference', ...
+    'Electromagnetic torque, \alpha_c = 2\pi600 rad/s, twice sampling frequency','Location', 'SouthEast');
+xlabel('Time (s)'); ylabel('Torque (Nm)');
